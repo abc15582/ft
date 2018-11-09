3f7c25ac02303e8e7deec8be00e48a50960a159f
yangpeng <abc15582@163.com>
2018-11-10 02:47:18 +0800

    init

---
+++ src/main/java/com/abc15582/FT/FtApplication.java
@@ -0,0 +1,12 @@
+package com.abc15582.FT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtApplication.class, args);
+	}
+}
